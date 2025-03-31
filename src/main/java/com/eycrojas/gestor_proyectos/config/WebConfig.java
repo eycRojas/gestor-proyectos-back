2dbb86b19a0d5eb8c1ebfe60b08d72c13d1f1f5f
Edwin Rojas <edwwinnerrojo@gmail.com>
2025-03-30 21:46:42 -0600

    Configuración de cors

--- src/main/java/com/eycrojas/gestor_proyectos/config/WebConfig.java
+++ src/main/java/com/eycrojas/gestor_proyectos/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Configura CORS globalmente
         registry.addMapping("/**")  // Aplícalo a todos los endpoints
-                .allowedOrigins("http://localhost:4200") // Permite solicitudes desde este origen (puedes agregar más si es necesario)
+                .allowedOrigins("http://localhost:4200", "https://gestor-proyectos-front.web.app") // Permite solicitudes desde este origen (puedes agregar más si es necesario)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
                 .allowedHeaders("*")  // Permite todos los encabezados
                 .allowCredentials(true);  // Permite enviar credenciales (cookies, autenticación, etc.)
